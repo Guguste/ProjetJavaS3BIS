f74079af099272fe563840afb3e94a176a02ef41
Hadjin <auguste.meyer@hotmail.fr>
2014-05-26 00:23:38 +0200

    ajout test interface

---
+++ testInterface/src/testInterface/Fenetre.java
@@ -0,0 +1,23 @@
+package testInterface;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame{
+
+	private String titre;
+	
+	private int x;
+	
+	private int y;
+	
+	public Fenetre(){
+		this.titre="test interface";
+		this.x=1600;
+		this.y=900;
+		this.setTitle(titre);
+		this.setSize(x, y);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+		this.setVisible(true);
+	}
+}
