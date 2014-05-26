b7670c0e977f54a5347de58a4335da5864f18c8b
Hadjin <auguste.meyer@hotmail.fr>
2014-05-26 11:15:12 +0200

    ajout test threads

--- Test/src/Start.java
+++ Test/src/Start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		
 		Fenetre f= new Fenetre();
+		System.out.println("Le nom du thread principal est " + Thread.currentThread().getName());
+	  
 
 	}
 
