cf313996f84782ace31433aadc23f779104cca6d
Hadjin <auguste.meyer@hotmail.fr>
2014-05-27 09:44:27 +0200

    test interface

--- Test/src/Fenetre.java
+++ Test/src/Fenetre.java
@@ -1,12 +1,19 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Fenetre extends JFrame{
+import javax.swing.*;
 
+
+public class Fenetre extends JFrame implements ActionListener{
+	
 	private String titre;
-	
 	private int x;
-	
 	private int y;
+	
+	private JButton b1, b2, b3, b4;
+	
+	private JPanel pan;
 	
 	public Fenetre(){
 		this.titre="test interface";
@@ -16,6 +23,25 @@ public class Fenetre extends JFrame{
 		this.setSize(x, y);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-		this.setVisible(true);
+		
+		
+		pan = new JPanel();
+		pan.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+		pan.add(b1 = new JButton("b1"));
+		pan.add(b2 = new JButton("b2"));
+		pan.add(b3 = new JButton("b3"));
+		pan.add(b4 = new JButton("b4"));
+		
+		Container contenu = getContentPane();
+		contenu.add(pan, BorderLayout.WEST);
+		
+		this.setVisible(true);	
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- Test/src/Start.java
+++ Test/src/Start.java
@@ -4,9 +4,6 @@ public class Start {
 	public static void main(String[] args) {
 		
 		Fenetre f= new Fenetre();
-		System.out.println("Le nom du thread principal est " + Thread.currentThread().getName());
-		System.out.println("Test java tango charly");
-
 	}
 
 }
