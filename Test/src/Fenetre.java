2834251fac681a2351eac7e555452cac88bf3186
Hadjin <auguste.meyer@hotmail.fr>
2014-05-30 18:38:51 +0200

    test truc horriblement chiant

--- Test/src/Fenetre.java
+++ Test/src/Fenetre.java
@@ -16,6 +16,7 @@ public class Fenetre extends JFrame implements ActionListener{
 	private JPanel pan;
 	
 	public Fenetre(){
+		//construction de la fenetre
 		this.titre="test interface";
 		this.x=1600;
 		this.y=900;
@@ -24,20 +25,55 @@ public class Fenetre extends JFrame implements ActionListener{
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
 		
+		//création de 3 panneaux fictif
+		JPanel p1 = new JPanel();
+		p1.setBackground(Color.BLUE);
+		JPanel p2 = new JPanel();
+		p2.setBackground(Color.RED);
+		JPanel p3 = new JPanel();
+		p3.setBackground(Color.GREEN);
+		JPanel p4 = new JPanel();
+		p3.setBackground(Color.PINK);
 		
-		pan = new JPanel();
-		pan.setBackground(Color.BLUE);
-		pan.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-		pan.add(b1 = new JButton("SAVE"));
-		pan.add(b2 = new JButton("Nouveau Media"));
-		pan.add(b3 = new JButton("Nouveau Livre"));
-		pan.add(b4 = new JButton("Nouveau CD"));
+		//declaration du panel qui contenir le tout
+		JPanel pano = new JPanel(new GridBagLayout());
 		
-		Container contenu = getContentPane();
-		contenu.add(pan, BorderLayout.WEST);
+		//déclaration du GridBagLayout
+		GridBagConstraints gbc = new GridBagConstraints();
 		
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx=0;
+		
+		gbc.gridwidth = 2;
+		gbc.gridy = 0;
+		gbc.gridheight = 2;
+		pano.add(p1, gbc);
+		 
+		gbc.gridx = 0;
+		gbc.gridwidth = 1;
+		gbc.gridy = 2;
+		gbc.gridheight = 2;
+		pano.add(p2, gbc);
+		 
+		gbc.gridx = 1;
+		gbc.gridwidth = 1;
+		gbc.gridy = 2;
+		gbc.gridheight = 2;
+		pano.add(p3, gbc);
+		 
+		gbc.gridx = 2;
+		gbc.gridwidth = 1;
+		gbc.gridy = 0;
+		gbc.gridheight = 4;
+		pano.add(p4, gbc);
+		
+		setContentPane(pano);
+		
+		
+		//rend le tout visible
 		this.setVisible(true);	
-		
 	}
 
 	@Override
@@ -45,4 +81,5 @@ public class Fenetre extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		
 	}
+ 
 }
